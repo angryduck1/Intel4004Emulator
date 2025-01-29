e5bfe9c3547618f2d443775957ad062ca91df241
angryduck001 <98224800+angryduck1@users.noreply.github.com>
2025-01-29 12:46:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <chrono>
 #include <thread>
+#include "assembler.h"
 
 class Intel4004 {
 public:
-    Intel4004(): pc(0), acc(0), rr0(0) {
-        memory = new int[256];
+    Intel4004(): pc(0), acc(0), rr0(0), rr1(0), flags(0) {
+        memory.resize(256);
     }
     
-    ~Intel4004() {
-        delete[] memory;
-    }
-    
-    void load_program(uint8_t instructions[6]) {
-        for (int i = 0; i < 256; i++) {
+    void load_program(std::vector<uint8_t> instructions) {
+        for (int i = 0; i < memory.size(); i++) {
             memory[i] = instructions[i];
         }
     }
@@ -22,29 +20,51 @@ public:
         return acc;
     }
     
+    uint8_t get_register0() {
+        return rr0;
+    }
+    
+    uint8_t get_register1() {
+        return rr1;
+    }
+    
     void run() {
         while (true) {
             uint8_t opcode = memory[pc++];
             switch (opcode) {
-                case 0x00: // Остановка
+                case NOP: // Остановка
                     return;
-                case 0xA2: // Загружаем число в аккум
+                case LDM: // Загружаем число в аккум
                     acc = memory[pc++];
                     break;
-                case 0xA4: // Загружаем число в регистер 0
-                    rr0 = memory[pc++];
+                case XCH_RR0: // Загружаем число в регистер 0 из аккума
+                    std::swap(acc, rr0);
+                    pc++;
                     break;
-                case 0x58: // Сложение
-                    acc += rr0;
-                    if (acc > 15) {
+                case XCH_RR1: // Загружаем число в регистер 1 из аккума
+                    std::swap(acc, rr1);
+                    pc++;
+                    break;
+                case ADD: // Сложение
+                    result = acc + rr0;
+                    if (result > 15) {
                         acc = 15;
+                        flags |= 0x01; // Устанавливаем флаг переноса
+                    } else {
+                        acc = result;
+                        flags &= ~0x01; // Сбрасываем флаг переноса
                     }
+                    pc++;
                     break;
-                case 0x29: // Вычитание
+                case SUB: // Вычитание
                     acc -= rr0;
                     if (acc < 0) {
                         acc = 0;
+                        flags |= 0x02;
+                    } else {
+                        flags &= ~0x02; // Сбрасываем флаг переноса
                     }
+                    pc++;
                     break;
                 default:
                     std::cerr << "Unknown instruction!" << std::endl;
@@ -56,26 +76,27 @@ public:
     
 private:
     uint8_t pc;
-    uint8_t rr0;
+    uint8_t rr0, rr1;
     uint8_t acc;
-    int* memory = nullptr;
+    uint8_t flags;
+    uint8_t result;
+    std::vector<int> memory;
 };
 
 int main() {
     Intel4004 ex;
     
-    uint8_t instructions[6] = {
-        0xA2, 0x05, // Помещаем в аккум значение 5
-        0xA4, 0x03, // Помещаем в регистер 0 значение 3
-        0x58, // Сложение
-        0x00
+    std::vector<uint8_t> instructions {
+        LDM, 0x05,
+        XCH_RR0
     };
     
     ex.load_program(instructions);
     
     ex.run();
     
-    std::cout << static_cast<int>(ex.get_accum()) << std::endl;
+    std::cout << static_cast<int>(ex.get_register0()) << std::endl;
     
     return 0;
+    
 }
