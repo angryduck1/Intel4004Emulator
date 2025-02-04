9f565cd62635241426ea7bbddffbccdc2dd0a126
angryduck001 <98224800+angryduck1@users.noreply.github.com>
2025-02-04 18:50:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <fstream>
 #include "assembler.h"
 #include "core.h"
 
 int main() {
     Intel4004 ex;
     
-    std::vector<uint8_t> instructions{
-            LDM, 9,         // Загружаем 9 в аккумулятор
-            XCH, RR0,       // Обмениваем значение аккумулятора с RR0 (RR0 теперь 9)
-            LDM, 5,         // Загружаем 8 в аккумулятор
-            ADD, RR0,       // Сложение: аккумулятор = 8 + 9 (аккумулятор теперь равен 17)
-            JCN, C1, 15,    // Если Carry установлен (переполнение), перейти к адресу 12
-            LDM, 8,         // Эта инструкция не будет выполнена при переходе
-            NOP,
-            LDM, 10         // Загружаем значение 10 в аккумулятор после перехода
-        };
+    std::ifstream inFile("main.txt"); // Put your file in this
+    
+    std::string line;
+    std::string asm_code;
+
+    while (std::getline(inFile, line)) {
+            asm_code += line + "\n";
+        }
+    
+    inFile.close();
+    
+    auto instructions = ex.assemble(asm_code);
     
     ex.load_program(instructions);
-    
     ex.run();
     
     ex.get_acc();
@@ -30,4 +32,3 @@ int main() {
     return 0;
     
 }
-
